893ea647019d27f2d4bdaa1d42ee87558aab831f
Martijn Stegeman <martijn@stgm.nl>
2022-10-05 09:53:57 +0200

    typecheck result layout

--- tests/_typecheck.py
+++ tests/_typecheck.py
@@ -11,7 +11,7 @@ def mypy_ok(test):
         return p.returncode == 0, p.stdout
 
     def report(output):
-        return 'line ' + '\n  - line '.join([':'.join(i.split(':')[1:]) for i in output.splitlines()[:-1]])
+        return '- line ' + '\n      - line '.join([':'.join(i.split(':')[1:]) for i in output.splitlines()[:-1]])
 
     test.description = lambda: "types are specified and correctly used"
     test.test = testMethod
