e15b9df7048a3184dd1092b2b11203a5c51d4ee5
Martijn Stegeman <martijn@stgm.nl>
2023-10-25 12:52:29 +0200

    calc extension

--- tests/calculatorTest.py
+++ tests/calculatorTest.py
@@ -4,7 +4,7 @@ import checkpy.assertlib as asserts
 
 from _extensions import *
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(10)
 def checks_answer0(test):
     """3 + 5 = 8.0"""
@@ -14,7 +14,7 @@ def checks_answer0(test):
         return asserts.exact(output.strip(), "8.0")
     test.test = testMethod
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(20)
 def checks_answer1(test):
     """19 - 6 = 13.0"""
@@ -24,7 +24,7 @@ def checks_answer1(test):
         return asserts.exact(output.strip(), "13.0")
     test.test = testMethod
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(30)
 def checks_answer2(test):
     """6 - 19 = -13.0"""
@@ -35,7 +35,7 @@ def checks_answer2(test):
     test.test = testMethod
 
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(40)
 def checks_answer3(test):
     """12 * 23 = 276.0"""
@@ -45,7 +45,7 @@ def checks_answer3(test):
         return asserts.exact(output.strip(), "276.0")
     test.test = testMethod
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(50)
 def checks_answer4(test):
     """6 / 4 = 1.5"""
@@ -55,7 +55,7 @@ def checks_answer4(test):
         return asserts.exact(output.strip(), "1.5")
     test.test = testMethod
 
-@t.passed(doctest_ok, hide=False)
+@t.passed(doctest_ok)
 @t.test(60)
 def checks_answer5(test):
     """-8 * 12 = -96.0"""
