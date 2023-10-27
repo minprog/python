87c1271d6c019c73330ed26ace845441fcb59692
jelleas <jelle.van.assema@gmail.com>
2023-10-27 11:25:02 +0200

    fix from import ban

--- tests/_static_analysis.py
+++ tests/_static_analysis.py
@@ -39,8 +39,9 @@ def has_import(*banned_imports) -> bool:
 
     imports_from: list[ast.ImportFrom] = static.getAstNodes(ast.ImportFrom)
     for imp in imports_from:
-        if imp.name in banned_imports:
-            return True
+        for name in imp.names:
+            if name in banned_imports:
+                return True
 
     return False
 
