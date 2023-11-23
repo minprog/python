961d7c0f58b9c71f372eaf08f531773097536482
Martijn Stegeman <martijn@stgm.nl>
2023-11-23 11:53:30 +0100

    errors

--- tests/linesTest.py
+++ tests/linesTest.py
@@ -4,11 +4,13 @@ from _static_analysis import *
 
 import ast
 
-@t.passed(doctest_ok)
-def test_point(test):
+@passed(doctest_ok)
+def test_point():
     """classes `Point` en `LineSegment` werken correct"""
     point1 = getModule().Point(1, 1)
     point2 = getModule().Point(3, 2)
     line_segment = getModule().LineSegment(point1, point2)
-    assert line_segment.slope() == 0.5
-    assert line_segment.length() == 2.23606797749979
+    if line_segment.slope() != 0.5:
+        raise AssertionError("expected line segment from (1, 1) to (3, 2) to have slope 0.5")
+    if line_segment.length() != 2.23606797749979:
+        raise AssertionError("expected line segment from (1, 1) to (3, 2) to have length 2.23606797749979")
