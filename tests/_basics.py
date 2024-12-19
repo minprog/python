77ee8d91a0c47500251a78b3866f6060753bc993
Martijn Stegeman <martijn@stgm.nl>
2024-12-19 09:04:56 +0100

    dontprint

--- tests/_basics.py
+++ tests/_basics.py
@@ -83,31 +83,31 @@ def mypy_ok(test):
 def doctest_ok(test):
     """doctests zijn voldoende aanwezig en geven allemaal akkoord"""
     def testMethod():
-        # with open(test.fileName, 'r') as source_file:
-        #     source = source_file.read()
-        #     functions = re.findall(r'def\s+(\w+)\(([^\)]*)\)[^-]+(->\s*([\w\[,\] _]+))?:', source)
-        #     n_functions_not_returning = len([function for function in functions if ('file' in function[1] or function[3] == 'None' or function[3] == '')])
-        #     n_functions = len(functions)
-        # p = subprocess.run([sys.executable or 'python3', '-m', 'doctest', '-v', test.fileName], capture_output=True, universal_newlines=True)
-        # if "Traceback" in p.stderr:
-        #     return False, p.stderr.splitlines()[-1]
-        # test_stats_rex = re.compile('(\d*) tests in (\d*) items')
-        # test_pass_rex = re.compile('(\d*) passed and (\d*) failed')
-        # test_stats = test_stats_rex.search(p.stdout.splitlines()[-3])
-        # test_pass = test_pass_rex.search(p.stdout.splitlines()[-2])
-        # n_tests = int(test_stats.group(1))
-        # n_items = int(test_stats.group(2))-1
-        # n_tested = n_items-n_functions_not_returning
-        # n_pass  = int(test_pass.group(1))
-        # if n_functions == 0:
-        #     return False, "je programma moet functies gebruiken (of type hints ontbreken!)"
-        # elif n_tested == 0:
-        #     # geen testbare functies blijkbaar?
-        #     return True
-        # elif n_tested > 0 and n_tests // n_tested < 2:
-        #     return False, f"{n_tests} voorbeelden bij {n_tested} testbare functies is niet genoeg \n    (we tellen alleen functies die iets returnen)"
-        # elif n_pass < n_tests:
-        #     return False, f"{n_pass} van {n_tests} voorbeelden slagen"
+        with open(test.fileName, 'r') as source_file:
+            source = source_file.read()
+            functions = re.findall(r'def\s+(\w+)\(([^\)]*)\)[^-]+(->\s*([\w\[,\] _]+))?:', source)
+            n_functions_not_returning = len([function for function in functions if ('file' in function[1] or function[3] == 'None' or function[3] == '')])
+            n_functions = len(functions)
+        p = subprocess.run([sys.executable or 'python3', '-m', 'doctest', '-v', test.fileName], capture_output=True, universal_newlines=True)
+        if "Traceback" in p.stderr:
+            return False, p.stderr.splitlines()[-1]
+        test_stats_rex = re.compile('(\d*) tests in (\d*) items')
+        test_pass_rex = re.compile('(\d*) passed and (\d*) failed')
+        test_stats = test_stats_rex.search(p.stdout.splitlines()[-3])
+        test_pass = test_pass_rex.search(p.stdout.splitlines()[-2])
+        n_tests = int(test_stats.group(1))
+        n_items = int(test_stats.group(2))-1
+        n_tested = n_items-n_functions_not_returning
+        n_pass  = int(test_pass.group(1))
+        if n_functions == 0:
+            return False, "je programma moet functies gebruiken (of type hints ontbreken!)"
+        elif n_tested == 0:
+            # geen testbare functies blijkbaar?
+            return True
+        elif n_tested > 0 and n_tests // n_tested < 2:
+            return False, f"{n_tests} voorbeelden bij {n_tested} testbare functies is niet genoeg \n    (we tellen alleen functies die iets returnen)"
+        elif n_pass < n_tests:
+            return False, f"{n_pass} van {n_tests} voorbeelden slagen"
         return True
 
     test.test = testMethod
