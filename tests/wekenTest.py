1fed934593d00a12dd39cef64510c36b1f817aa0
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-11 13:54:04 +0200

    Update wekenTest.py

--- tests/wekenTest.py
+++ tests/wekenTest.py
@@ -9,14 +9,14 @@ def has_functions():
     """functie `weeks_elapsed` is aanwezig"""
     assert defines_function("weeks_elapsed")
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_weeks_elapsed(test):
     """functie `weeks_elapsed` werkt correct"""
     assert getFunction("weeks_elapsed")(3,20) == 2
     assert getFunction("weeks_elapsed")(20,3) == 2
     assert getFunction("weeks_elapsed")(1,1) == 0
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_program(test):
     """het programma werkt correct met invoer en uitvoer"""
     assert outputOf(stdinArgs=[3,20], overwriteAttributes=[("__name__", "__main__")]) == "Er zijn 2 volle weken verstreken.\n"
