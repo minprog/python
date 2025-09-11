7d7f6e94949fb3ac1253a6f54e79b2058856c21e
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-11 11:30:35 +0200

    Update binairTest.py

--- tests/binairTest.py
+++ tests/binairTest.py
@@ -9,7 +9,7 @@ def has_functions():
     """functie `convert` is aanwezig"""
     assert defines_function("convert")
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_convert(test):
     """functie `convert` werkt correct"""
     assert getFunction("convert")(0, 1, 0, 0) == 4
@@ -17,7 +17,7 @@ def test_convert(test):
     assert getFunction("convert")(0, 1, 0, 1) == 5
     assert getFunction("convert")(0, 0, 0, 0) == 0
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_program(test):
     """het programma werkt correct met invoer en uitvoer (0111)"""
     assert outputOf(stdinArgs=[0, 1, 1, 1], overwriteAttributes=[("__name__", "__main__")]) == "7\n"
