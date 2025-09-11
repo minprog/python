8108b08c35ecc4c4ef3bdd9db986de351b8a7291
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-11 11:29:28 +0200

    Update waterTest.py

--- tests/waterTest.py
+++ tests/waterTest.py
@@ -9,7 +9,7 @@ def has_functions():
     """functie `hoeveelheid_water` is aanwezig"""
     assert defines_function("hoeveelheid_water")
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_hoeveelheid_water(test):
     """functie `hoeveelheid_water` werkt correct"""
     assert getFunction("hoeveelheid_water")(1) == 12
@@ -17,7 +17,7 @@ def test_hoeveelheid_water(test):
     assert getFunction("hoeveelheid_water")(20) == 240
     assert getFunction("hoeveelheid_water")(0) == 0
 
-@t.passed(has_functions)
+@passed(has_functions)
 def test_program(test):
     """het programma werkt correct met invoer en uitvoer (21 minuten)"""
     assert outputOf(stdinArgs=[21], overwriteAttributes=[("__name__", "__main__")]) == "252\n"
