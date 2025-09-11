6991e33b52718e5717e27bd471992cde25ddc1ae
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-11 12:07:19 +0200

    fixes

--- tests/colaTest.py
+++ tests/colaTest.py
@@ -26,35 +26,28 @@ def has_functions():
 @test(10)
 def checks_coin(test):
     """functie `check_coin` werkt correct"""
-    def testMethod():
-        coin = lib.getFunction("check_coin", test.fileName)
-        if coin(25) and coin(10) and coin(5) and not coin(1) and not coin("10"):
-            return True
-        else:
-            return False
-    test.test = testMethod
+    coin = getFunction("check_coin", test.fileName)
+    assert_return(True, coin, 25)
+    assert_return(True, coin, 10)
+    assert_return(True, coin, 5)
+    assert_return(False, coin, 1)
+    assert_return(False, coin, 2)
+    assert_return(False, coin, "10")
 
 @passed(has_functions)
 @test(20)
 def checks_due(test):
     """functie `determine_due` werkt correct"""
-    def testMethod():
-        due = lib.getFunction("determine_due", test.fileName)
-        if due(50, 20) == 30:
-            return False, "functie accepteert een munt van 20 cent, maar deze bestaat niet"
-        if due(50, 25) == 25 and due(50, 20) == 50 and due(50, 10) == 40:
-            return True
-        else:
-            return False
-    test.test = testMethod
+    due = getFunction("determine_due", test.fileName)
+    if due(50, 20) == 30:
+        raise AssertionError("functie accepteert een munt van 20 cent, maar deze bestaat niet")
+    assert_return(25, due, 50, 25)
+    assert_return(50, due, 50, 20)
+    assert_return(40, due, 50, 10)
 
 @passed(has_functions)
 @test(30)
 def checks_main(test):
     """wisselgeld van 10 cent wordt verkregen na invoeren van 25, 10, en dan 25 cent"""
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=[25, 10, 25],
-            overwriteAttributes = [("__name__", "__main__")])
-        answer = "10"
-        return asserts.exact(output.strip()[-2:], answer)
-    test.test = testMethod
+    output = run(25, 10, 25)
+    assert output.strip()[-2:] == "10"
