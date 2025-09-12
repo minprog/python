e3b55cceea56b44e354611896e56e198d316e1c9
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-12 20:05:59 +0200

    Update greedyTest.py

--- tests/greedyTest.py
+++ tests/greedyTest.py
@@ -11,9 +11,9 @@ forbidden_constructs.disallow_all()
 @passed(checkstyle, forbidden_constructs, mypy_strict, doctest)
 def has_functions():
     """alle gevraagde functies zijn aanwezig"""
+    assert not_in_code(ast.List)
     assert not_in_code(ast.For) # for-loop makes no sense here
     assert not_in_code(ast.Set)
-    assert not_in_code(ast.List)
     assert not_in_code(ast.Tuple)
     assert not_in_code(ast.Dict)
     assert not_in_code(ast.In)
