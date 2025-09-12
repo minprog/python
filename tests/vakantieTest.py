1db88cd6092ed4a85b559a2857f9973e0dcf9203
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-12 19:15:42 +0200

    Update vakantieTest.py

--- tests/vakantieTest.py
+++ tests/vakantieTest.py
@@ -1,10 +1,6 @@
 from checkpy import *
 from _static_analysis import *
 
-# TODO modernize
-import checkpy.lib as lib
-import checkpy.assertlib as asserts
-
 from _python_checks import checkstyle, forbidden_constructs, mypy_strict, doctest
 forbidden_constructs.disallow_all()
 
@@ -16,70 +12,41 @@ def has_functions():
 @passed(has_functions)
 @test(10)
 def calculatesTravelCostsWithHint(test):
-    """functie 'travel_costs' berekent correct de vervoerkosten"""
-    def testMethod():
-        travelCosts = lib.getFunction("travel_costs", test.fileName, stdinArgs=[1000, 0])(1000)
-        if travelCosts == 130:
-            return (False,
-                    "vergeet niet om de kosten voor zowel heen als terug te berekenen"
-            )
-        elif travelCosts == 260:
-            return True
-        else:
-            return False
-    test.test = testMethod
+    """functie `travel_costs` berekent correct de vervoerkosten"""
+    result = get_function("travel_costs")(1000)
+    if result == 130:
+        raise AssertionError("vergeet niet om de kosten voor zowel heen als terug te berekenen")
+    elif result == 260:
+        return True
+    else:
+        raise AssertionError
 
 @passed(has_functions)
 @test(20)
 def calculatesZeroCosts(test):
-    """print correct 'Jouw vakantie kost: 0' bij [0, 0] als invoer"""
-    target = "0"
-    args = [0, 0]
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=args, overwriteAttributes = [("__name__", "__main__")])
-        return asserts.contains(output.strip(), target)
-    test.test = testMethod
+    """run: print correct 'Jouw vakantie kost: 0' bij [0, 0] als invoer"""
+    assert_output(run(0, 0).number(), '0')
 
 @passed(has_functions)
 @test(20)
 def calculatesTravelCosts(test):
-    """print correct 'Jouw vakantie kost: 260' bij [1000, 0] als invoer"""
-    target = "260"
-    args = [1000, 0]
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=args, overwriteAttributes = [("__name__", "__main__")])
-        return asserts.contains(output.strip(), target)
-    test.test = testMethod
+    """run: print correct 'Jouw vakantie kost: 260' bij [1000, 0] als invoer"""
+    assert_output(run(1000, 0).number(), '260')
 
 @passed(has_functions)
 @test(30)
 def calculatesSleepingCosts(test):
-    """print correct 'Jouw vakantie kost: 600' bij [0, 10] als invoer"""
-    target = "600"
-    args = [0, 10]
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=args, overwriteAttributes = [("__name__", "__main__")])
-        return asserts.contains(output.strip(), target)
-    test.test = testMethod
+    """run: print correct 'Jouw vakantie kost: 600' bij [0, 10] als invoer"""
+    assert_output(run(0, 10).number(), '600')
 
 @passed(has_functions)
 @test(40)
 def calculatesCosts(test):
-    """print correct 'Jouw vakantie kost: 589' bij [650, 7] als invoer"""
-    target = "589"
-    args = [650, 7]
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=args, overwriteAttributes = [("__name__", "__main__")])
-        return asserts.contains(output.strip(), target)
-    test.test = testMethod
+    """run: print correct 'Jouw vakantie kost: 589' bij [650, 7] als invoer"""
+    assert_output(run(650, 7).number(), '589')
 
 @passed(has_functions)
 @test(50)
 def calculatesCostsAndRoundsCorrectly(test):
-    """print correct 'Jouw vakantie kost: 371' bij [1425, 0] als invoer"""
-    target = "371"
-    args = [1425, 0]
-    def testMethod():
-        output = lib.outputOf(test.fileName, stdinArgs=args, overwriteAttributes = [("__name__", "__main__")])
-        return asserts.contains(output.strip(), target)
-    test.test = testMethod
+    """run: print correct 'Jouw vakantie kost: 371' bij [1425, 0] als invoer"""
+    assert_output(run(1425, 0).number(), '371')
