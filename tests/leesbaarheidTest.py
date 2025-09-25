c72c1ecebd06866c01fe9fc628b202565b23263a
Martijn Stegeman <m.stegeman@uva.nl>
2025-09-25 12:23:02 +0200

    modernize

--- tests/leesbaarheidTest.py
+++ tests/leesbaarheidTest.py
@@ -18,41 +18,33 @@ def has_functions():
     assert no_string_methods_used()
 
 @passed(has_functions)
-@test(10)
 def checks_coleman_liau(test):
     """functie `coleman_liau` werkt correct"""
-    def testMethod():
-        coleman_liau = getFunction("coleman_liau", test.fileName)
-        if coleman_liau(100, 5, 500) == 12:
-            return True
-        else:
-            return False
-    test.test = testMethod
+    coleman_liau = get_function("coleman_liau")
+    assert coleman_liau(100, 5, 500) == 12
 
 @passed(has_functions)
-@test(20)
 def checks_calculate_grade(test):
     """functie `calculate_grade` werkt correct"""
-    def testMethod():
-        calculate_grade = getFunction("calculate_grade", test.fileName)
-        if calculate_grade("One fish. Two fish. Red fish. Blue fish.") == -9:
-            return True
-        else:
-            return False
-    test.test = testMethod
+    calculate_grade = get_function("calculate_grade")
+    assert calculate_grade("One fish. Two fish. Red fish. Blue fish.") == -9
 
 @passed(has_functions)
-@test(30)
 def checks_tekst1(test):
     """geeft Grade 7 voor "In my younger and more..." """
-    output = outputOf(test.fileName, stdinArgs=["In my younger and more vulnerable years my father gave me some advice that I've been turning over in my mind ever since."],
-        overwriteAttributes = [("__name__", "__main__")])
-    assert output.strip() == "Grade 7", "zorg dat de output exact zo is als in de voorbeelden"
+    run(
+        "In my younger and more vulnerable years my father gave me "
+        "some advice that I've been turning over in my mind ever since."
+    ).strip() == "Grade 7"
+    # hint: "zorg dat de output exact zo is als in de voorbeelden"
 
 @passed(has_functions)
-@test(40)
 def checks_tekst2(test):
     """geeft Grade 10 voor "It was a bright cold day..." """
-    output = outputOf(test.fileName, stdinArgs=["It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him."],
-        overwriteAttributes = [("__name__", "__main__")])
-    assert output.strip() == "Grade 10"
+    run(
+        "It was a bright cold day in April, and the clocks were striking "
+        "thirteen. Winston Smith, his chin nuzzled into his breast in an "
+        "effort to escape the vile wind, slipped quickly through the glass "
+        "doors of Victory Mansions, though not quickly enough to prevent a "
+        "swirl of gritty dust from entering along with him."
+    ).strip() == "Grade 10"
