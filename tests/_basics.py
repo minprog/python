62a6e7d4d8d40e7ff41f97003fa3f68695e3f55e
Martijn Stegeman <m.stegeman@uva.nl>
2025-10-08 10:51:31 +0200

    pull new base checks into old base check

--- tests/_basics.py
+++ tests/_basics.py
@@ -73,50 +73,6 @@ def basic_style(test):
         return True
     test.test = testMethod
 
-@t.passed(basic_style, hide=False)
-@t.test(2)
-def mypy_ok(test):
-    """type hints zijn ingevuld en consistent bevonden"""
-    def testMethod():
-        p = subprocess.run(['mypy', '--strict', '--ignore-missing-imports', '--disable-error-code=name-defined', test.fileName], capture_output=True, universal_newlines=True)
-        return p.returncode == 0, p.stdout
-    test.test = testMethod
-    def report(output):
-        return '- line ' + '\n- line '.join([':'.join(i.split(':')[1:])[:60] for i in output.splitlines()[:-1]])
-    test.fail = report
+from _mypy_strict import mypy_ok
 
-@t.passed(mypy_ok, hide=False)
-@t.test(3)
-def doctest_ok(test):
-    """doctests zijn voldoende aanwezig en geven allemaal akkoord"""
-    def testMethod():
-        with open(test.fileName, 'r') as source_file:
-            source = source_file.read()
-            functions = re.findall(r'def\s+(\w+)\(([^\)]*)\)[^-]+(->\s*([\w\[,\] _]+))?:', source)
-            n_functions_not_returning = len([function for function in functions if ('file' in function[1] or function[3] == 'None' or function[3] == '')])
-            n_functions = len(functions)
-        p = subprocess.run([sys.executable or 'python3', '-m', 'doctest', '-v', test.fileName], capture_output=True, universal_newlines=True)
-        if "Traceback" in p.stderr:
-            return False, p.stderr.splitlines()[-1]
-        test_stats_rex = re.compile('(\d*) tests in (\d*) items')
-        test_pass_rex = re.compile('(\d*) passed and (\d*) failed')
-        test_stats = test_stats_rex.search(p.stdout.splitlines()[-3])
-        test_pass = test_pass_rex.search(p.stdout.splitlines()[-2])
-        n_tests = int(test_stats.group(1))
-        n_items = int(test_stats.group(2))-1
-        n_tested = n_items-n_functions_not_returning
-        n_pass  = int(test_pass.group(1))
-        if n_functions == 0:
-            return False, "je programma moet functies gebruiken (of type hints ontbreken!)"
-        elif n_tested == 0:
-            # geen testbare functies blijkbaar?
-            return True
-        elif n_tested > 0 and n_tests // n_tested < 2:
-            return False, f"{n_tests} voorbeelden bij {n_tested} testbare functies is niet genoeg \n    (we tellen alleen functies die iets returnen)"
-        elif n_pass < n_tests:
-            return False, f"{n_pass} van {n_tests} voorbeelden slagen"
-        return True
-
-    test.test = testMethod
-    test.fail = lambda info: info
-    test.timeout = lambda: 120
+from _check_doctests import require_doctests_for_returning_functions as doctest_ok
