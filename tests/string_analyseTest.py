0f78d6b4bedd9bb3aae2bd3cac6a417117de4031
Martijn Stegeman <m.stegeman@uva.nl>
2025-10-28 15:55:52 +0100

    checks

--- tests/string_analyseTest.py
+++ tests/string_analyseTest.py
@@ -17,3 +17,68 @@ def has_functions():
     assert construct_not_in_ast(ast.Tuple)
     assert construct_not_in_ast(ast.Dict)
     # assert construct_not_in_ast(ast.In)
+
+@passed(has_functions)
+def test_function_isspace(test):
+    """functie `isspace` werkt correct"""
+    isspace = get_function("isspace")
+    assert isspace(" ") == True
+    assert isspace("\t") == True
+    assert isspace("\n") == True
+    assert isspace("\\n") == False
+    assert isspace("A") == False
+    assert isspace("a") == False
+    assert isspace("space") == False
+    assert isspace("") == False
+
+@passed(has_functions)
+def test_function_isvowel(test):
+    """functie `isvowel` werkt correct"""
+    isvowel = get_function("isvowel")
+    assert isvowel("a") == True
+    assert isvowel("e") == True
+    assert isvowel("i") == True
+    assert isvowel("o") == True
+    assert isvowel("u") == True
+    assert isvowel("y") == False
+    assert isvowel("aa") == True
+    assert isvowel("slim") == False
+    assert isvowel("im") == False
+    assert isvowel("imma") == False
+    assert isvowel("A") == False
+    assert isvowel("aeiou") == True
+    assert isvowel("") == True
+
+@passed(has_functions)
+def test_function_has_single_vowel(test):
+    """functie `has_single_vowel` werkt correct"""
+    has_single_vowel = get_function("has_single_vowel")
+    assert has_single_vowel("a") == True
+    assert has_single_vowel("e") == True
+    assert has_single_vowel("i") == True
+    assert has_single_vowel("o") == True
+    assert has_single_vowel("u") == True
+    assert has_single_vowel("y") == False
+    assert has_single_vowel("bims") == True
+    assert has_single_vowel("smib") == True
+    assert has_single_vowel("yolo") == False
+    assert has_single_vowel("rough") == False
+    assert has_single_vowel("") == False
+
+@passed(has_functions)
+def test_function_count_vowels(test):
+    """functie `count_vowels` werkt correct"""
+    count_vowels = get_function("count_vowels")
+    assert count_vowels("a") == 1
+    assert count_vowels("e") == 1
+    assert count_vowels("i") == 1
+    assert count_vowels("o") == 1
+    assert count_vowels("u") == 1
+    assert count_vowels("y") == 0
+    assert count_vowels("t") == 0
+    assert count_vowels("A") == 0
+    assert count_vowels("bam") == 1
+    assert count_vowels("space") == 2
+    assert count_vowels("yolo") == 2
+    assert count_vowels("hoedan") == 3
+    assert count_vowels("") == 0
--- tests/string_verschillenTest.py
+++ tests/string_verschillenTest.py
@@ -15,3 +15,33 @@ def has_functions():
     assert construct_not_in_ast(ast.Tuple)
     assert construct_not_in_ast(ast.Dict)
     assert construct_not_in_ast(ast.In)
+
+@passed(has_functions)
+def test_function_is_different(test):
+    """functie `is_different` werkt correct"""
+    is_different = get_function("is_different")
+    assert is_different("dance", "night") == True
+    assert is_different("dance", "dance") == False
+    assert is_different("a", "e") == True
+    assert is_different("a", "a") == False
+    assert is_different("A", "a") == True
+    assert is_different("jumpstyle", "hakken") == True
+    assert is_different("piano", "pianissimo") == True
+    assert is_different("", "mamba") == True
+    assert is_different("mamba", "") == True
+    assert is_different("", "") == False
+
+@passed(has_functions)
+def test_function_count_difference(test):
+    """functie `count_difference` werkt correct"""
+    count_difference = get_function("count_difference")
+    assert count_difference("dance", "night") == 5
+    assert count_difference("dance", "dance") == 0
+    assert count_difference("a", "e") == 1
+    assert count_difference("a", "a") == 0
+    assert count_difference("A", "a") == 1
+    assert count_difference("jumpstyle", "hakken") == 9
+    assert count_difference("piano", "pianissimo") == 10
+    assert count_difference("", "mamba") == 5
+    assert count_difference("mamba", "") == 5
+    assert count_difference("", "") == 0
