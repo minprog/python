a6f16f4d6b615f316e2bd8e2e632afeba1a899eb
Martijn Stegeman <m.stegeman@uva.nl>
2025-11-17 09:30:40 +0100

    c

--- tests/shoutTest.py
+++ tests/shoutTest.py
@@ -1,3 +1,4 @@
+from ast import Assert
 from checkpy import *
 from _pyprog_tools import *
 
@@ -16,17 +17,27 @@ def has_functions():
     assert no_string_mult_used()
     assert no_string_methods_used()
 
+class SafeString(str):
+    def lower(self):
+        raise AssertionError("je mag geen lower gebruiken voor de hele string!")
+    def upper(self):
+        raise AssertionError("je mag geen upper gebruiken voor de hele string!")
+
 @passed(has_functions)
-def test_function(test):
+def test_function():
     """functie `shout` werkt correct"""
     shout = get_function("shout")
     assert shout("Who the hell put the muffins the freezer?") == \
                  "WHO THE HELL PUT THE MUFFINS THE FREEZER?"
     assert shout("") == ""
     assert shout("let it all out...") == "LET IT ALL OUT..."
+    try:
+        assert shout(SafeString("hoi")) == "HOI"
+    except BaseException as e:
+        raise AssertionError("je mag geen upper/lower gebruiken voor de hele string, wel voor 1 teken")
 
 @passed(has_functions)
-def test_program(test):
+def test_program():
     """het programma werkt correct met invoer en uitvoer"""
     assert run("Who the hell put the muffins the freezer?") == \
                "WHO THE HELL PUT THE MUFFINS THE FREEZER?\n"
