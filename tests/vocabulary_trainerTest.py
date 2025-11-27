acbef187dfaa2caec79b1cc6307f805128a1bdc9
Martijn Stegeman <m.stegeman@uva.nl>
2025-11-27 17:25:55 +0100

    fix

--- tests/vocabulary_trainerTest.py
+++ tests/vocabulary_trainerTest.py
@@ -12,36 +12,41 @@ def test_class():
 
 @passed(test_class)
 def test_add_and_get():
-    """"""
+    """één woord toevoegen en dan de vertaling opzoeken"""
     VocabularyTrainer = getModule().VocabularyTrainer
     vt = VocabularyTrainer()
     vt.add_word("huis", "house")
-    assert vt.get_translation("huis") == ["house"]
+    assert vt.get_translation("huis") in [ {"house"},  ["house"] ]
 
 @passed(test_class)
 def test_add_duplicates():
-    """"""
+    """twee vertalingen van één woord toevoegen en dan opzoeken"""
     VocabularyTrainer = getModule().VocabularyTrainer
     vt = VocabularyTrainer()
     vt.add_word("huis", "house")
     vt.add_word("huis", "home")
     # order doesn't matter
-    assert set(vt.get_translation("huis")) == {"house", "home"}
+    assert set(vt.get_translation("huis")) in [ {"house", "home"}, ["house", "home"] ]
 
 @passed(test_class)
 def test_remove():
-    """"""
+    """één woord toevoegen, dan verwijderen; opzoeken van dat woord geeft een error"""
     VocabularyTrainer = getModule().VocabularyTrainer
     vt = VocabularyTrainer()
     vt.add_word("boom", "tree")
     vt.remove_word("boom")
-    assert vt.get_translation("boom") == None
+    raised = False
+    try:
+        vt.get_translation("boom")
+    except KeyError:
+        raised = True
+    assert raised, "add_contact moet KeyError geven bij duplicate key"
 
 @passed(test_class)
 def test_list_words():
-    """"""
+    """twee woorden toevoegen; `list_words` moet dan die woorden geven"""
     VocabularyTrainer = getModule().VocabularyTrainer
     vt = VocabularyTrainer()
     vt.add_word("a", "1")
     vt.add_word("b", "2")
-    assert set(vt.list_words()) == {"a", "b"}
+    assert vt.list_words() in [ {"a", "b"}, ["a", "b"] ]
